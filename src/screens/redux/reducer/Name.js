c4d88fbc2fc42a10c4213ac51ccc4b07e5b34a79
Shahnawaz Hussain <shahnawazhussain125@gmail.com>
2020-03-23 13:29:08 +0500

    commit

--- src/screens/redux/reducer/Name.js
+++ src/screens/redux/reducer/Name.js
@@ -1,6 +1,6 @@
 const Name = (state = "anees", action) => {
   console.log("state of name reducer", state);
-  console.log("action of nmae reducer", action);
+  console.log("action of nmae reducer", action.type);
 
   return state;
 };
--- src/screens/redux/reducer/Wishes.js
+++ src/screens/redux/reducer/Wishes.js
@@ -1,6 +1,6 @@
 export const Wishes = (state = ["eat", "sleep", "drink"], action) => {
   console.log("state of wishles reducer", state);
-  console.log("action of wishles reducer", action);
+  console.log("action of wishles reducer", action.type);
 
   return state;
 };
